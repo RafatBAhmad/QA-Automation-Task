8380453249e54f3f60dff0aa432715b2aa87c763
‪Rafat Ahmad‬‏ <37984571+RafatBAhmad@users.noreply.github.com>
2021-12-28 12:24:38 +0200

    changeFileName_addObjToSelectors

--- cypress/support/hotels_helper.js
+++ cypress/support/hotels_helper.js
@@ -1,9 +1,14 @@
 const SELECTORS = {
     searchField: "[type = 'text']",
-    searchButton: "[class ='_3pFoIe eLsxCc']"
+    searchButton: "[class ='_3pFoIe eLsxCc']",
+    locationInfo: "._2lmU8j",
+    hotelsSearchResult: '._61P-R0',
+    dataLocationInfo: 'span'
 }
 /**
- * Method DOM
+ * This method type in searchField and then search for the result after that
+ * open the first result and check if the location of this result is in the same
+ * searched location.
  * @param {String} searchVal 
  * 
  */
@@ -18,11 +23,11 @@ export const search = (searchVal) => {
     cy.wait('@searchForTerms', { timeout: 30000 })
     // cy.get("#0").click()
 
-    cy.get('._61P-R0').eq(0).invoke('removeAttr', 'target').click()
+    cy.get(SELECTORS.hotelsSearchResult).eq(0).invoke('removeAttr', 'target').click()
     // cy.pause()
     //cy.contains("span", searchVal)
-    cy.get("._2lmU8j").within(() => {
-        cy.get('span').invoke('text').then((ActualTextLocation)=>{
+    cy.get(SELECTORS.locationInfo).within(() => {
+        cy.get(SELECTORS.dataLocationInfo).invoke('text').then((ActualTextLocation)=>{
             if (!ActualTextLocation.includes(searchVal)){
                 expect(ActualTextLocation).to.include(searchVal)
 
