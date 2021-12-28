eff31e92ae293b0c030df8e02f119a487cafcbdf
‪Rafat Ahmad‬‏ <37984571+RafatBAhmad@users.noreply.github.com>
2021-12-28 11:41:49 +0200

    Update main.spec.js

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -2,7 +2,7 @@
 
 import { search } from "../support/hotels_helper"
 
-const POS_SEARCH_LOCATION = "Nablus";
+const POS_SEARCH_LOCATION = "Jerusalem";
 const NEG_SEARCH_LOCATION = "$1454_Nablus";
 
 describe("Verify that search filters works well", () => {
