05447d82fbf5891a34f89508dc5f63692de2964f
‪Rafat Ahmad‬‏ <37984571+RafatBAhmad@users.noreply.github.com>
2022-01-06 12:46:50 +0200

    solution with wrong format

--- cypress/integration/hotel_search.spec.js
+++ cypress/integration/hotel_search.spec.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 
-import { search } from "../support/hotels_helper"
+import { search_positive } from "../support/hotels_helper"
+import { search_negative } from "../support/hotels_helper"
 
-const POS_SEARCH_LOCATION = "Jerusalem";
-const NEG_SEARCH_LOCATION = "$1454_Nablus";
+const POS_SEARCH_LOCATION = 'Jerusalem';
+const NEG_SEARCH_LOCATION = '$12!3_Jerusalem';
 
 describe("Verify that search filters works well", () => {
     beforeEach(() => {
         cy.visit("https://www.hotels.com/", {
             headers: {
-                "Accept-Encoding": "gzip, deflate"
+                "Accept-Encoding": "gzip, deflate"    
             }
-        })
-        
+        })        
     })
-    it("Positive Test Case", () => {
-        search(POS_SEARCH_LOCATION)
+    it("Test correct format address location in the search location field", () => {
+        search_positive(POS_SEARCH_LOCATION)
     })
 
-    it("Negative Test Case", () => {
-        search(NEG_SEARCH_LOCATION)
+    it("Test wrong format address location in the search location field", () => {
+        search_negative(NEG_SEARCH_LOCATION)
     })
 
 })
--- cypress/support/hotels_helper.js
+++ cypress/support/hotels_helper.js
@@ -6,34 +6,54 @@ const SELECTORS = {
     dataLocationInfo: 'span'
 }
 /**
- * This method type in searchField and then search for the result after that
+ * These methods type in searchField and then search for the result after that
  * open the first result and check if the location of this result is in the same
- * searched location.
+ * searched location.The negative means that the searchVal is in wrong format.
  * @param {String} searchVal 
  * 
  */
-export const search = (searchVal) => {
+export const search_negative = (searchVal) => {
+    const type_enter = searchVal + '{enter}'
+    cy.get(SELECTORS.searchField).type(type_enter)
+    let new_location_name
+    let final_location
+    cy.get(SELECTORS.searchField).then($el => {
+        new_location_name = $el.text()
+        final_location = new_location_name.split(",")
+        cy.wait(10000)
+        click_search(final_location[0])
+    })
+    //cy.wait(10000)
+    // click_search(f_l)
+    
+
+}
+
+
+export const search_positive = (searchVal) => {
+    cy.get(SELECTORS.searchField).type(searchVal)
+    cy.contains(searchVal).click()
+    click_search(searchVal)
+}
+
+/**
+ * search and chose hotel and check if the chosen one is in the same search loaction.
+ * @param {String} searchVal
+ *
+ */
+export const click_search = (searchVal) => {
     cy.intercept({
         method: "GET",
         url: "https://securepubads.g.doubleclick.net/gampad/*"
     }).as('searchForTerms')
-    cy.get(SELECTORS.searchField).type(searchVal)
-    cy.contains(searchVal).click()
     cy.get(SELECTORS.searchButton).click()
     cy.wait('@searchForTerms', { timeout: 30000 })
-    // cy.get("#0").click()
-
     cy.get(SELECTORS.hotelsSearchResult).eq(0).invoke('removeAttr', 'target').click()
-    // cy.pause()
-    //cy.contains("span", searchVal)
     cy.get(SELECTORS.locationInfo).within(() => {
-        cy.get(SELECTORS.dataLocationInfo).invoke('text').then((ActualTextLocation)=>{
-            if (!ActualTextLocation.includes(searchVal)){
+        cy.get(SELECTORS.dataLocationInfo).invoke('text').then((ActualTextLocation) => {
+            if (!ActualTextLocation.includes(searchVal)) {
                 expect(ActualTextLocation).to.include(searchVal)
-
             }
-
         })
     })
-    
 }
